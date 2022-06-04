6113f0eaef06a0b70470c6646feddc5fa1bee54f
sachinlandge004 <sachinlandge004@gmail.com>
2022-06-05 00:42:23 +0530

    corrected

--- 450-DSA/src/arrays/FindMissingNumber.java
+++ 450-DSA/src/arrays/FindMissingNumber.java
@@ -3,20 +3,25 @@ package arrays;
 public class FindMissingNumber {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		int arr[] = {1,2,4,5};
-		
-		int result = findMissing(arr,5);
+		int arr[] = { 1, 2, 4, 5 };
+		System.out.println(findMissing(arr)); 
 
 	}
 
-	private static int findMissing(int[] arr,int size) {
+	private static int findMissing(int[] nums) {
+
+		/*
+		 * Calculate the sum of first n natural numbers as sumtotal= n*(n+1)/2 Create a
+		 * variable sum to store the sum of array elements. Traverse the array from
+		 * start to end. Update the value of sum as sum = sum + array[i] Print the
+		 * missing number as sumtotal – sum
+		 */
 		
-		int totalSum = size*(size+1/2);
-		int sum =0;
-		//for 
-		return 0;
+		int n = nums.length;
+		int sum = ((n + 1) * (n + 2)) / 2;
+		for (int i = 0; i < n; i++)
+			sum -= nums[i];
+		return sum;
 	}
 
 }
